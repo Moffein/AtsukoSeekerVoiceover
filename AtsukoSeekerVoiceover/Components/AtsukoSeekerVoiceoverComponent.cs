4605e885f7e60fe61a414a7df2c27718cb264716
Moffein <22742925+Moffein@users.noreply.github.com>
2024-11-01 15:41:24 -0500

    Auto Voicelines work.

---
+++ AtsukoSeekerVoiceover/Components/AtsukoSeekerVoiceoverComponent.cs
@@ -0,0 +1,270 @@
+﻿using BaseVoiceoverLib;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace AtsukoSeekerVoiceover.Components
+{
+    public class AtsukoSeekerVoiceoverComponent : BaseVoiceoverComponent
+    {
+        private bool acquiredScepter = false;
+
+        private float levelCooldown = 0f;
+        private float blockedCooldown = 0f;
+        private float lowHealthCooldown = 0f;
+        private float specialCooldown = 0f;
+        private float shrineFailCooldown = 0f;
+
+        public static NetworkSoundEventDef nseBlock, nseShout, nseShrineFail, nseMove, nseMoveLong, nseEx1, nseEx2, nseEx3, nseExL1, nseExL2, nseExL3;
+
+        protected override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            if (specialCooldown > 0f) specialCooldown -= Time.fixedDeltaTime;
+            if (levelCooldown > 0f) levelCooldown -= Time.fixedDeltaTime;
+            if (blockedCooldown > 0f) blockedCooldown -= Time.fixedDeltaTime;
+            if (lowHealthCooldown > 0f) lowHealthCooldown -= Time.fixedDeltaTime;
+            if (shrineFailCooldown > 0f) shrineFailCooldown -= Time.fixedDeltaTime;
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+            if (inventory && inventory.GetItemCount(scepterIndex) > 0) acquiredScepter = true;
+        }
+        public override void PlayLevelUp()
+        {
+            if (levelCooldown > 0f) return;
+            bool played;
+            if (Util.CheckRoll(50f))
+            {
+                if (Util.CheckRoll(50f))
+                {
+                    played = TryPlaySound("Play_AtsukoSeeker_LevelUp_1", 7f, false);
+                }
+                else
+                {
+                    played = TryPlaySound("Play_AtsukoSeeker_LevelUp_2", 6f, false);
+                }
+            }
+            else
+            {
+                if (Util.CheckRoll(50f))
+                {
+                    played = TryPlaySound("Play_AtsukoSeeker_LevelUp_3", 4.1f, false);
+                }
+                else
+                {
+                    played = TryPlaySound("Play_AtsukoSeeker_LevelUp_4", 2.7f, false);
+                }
+            }
+            if (played) levelCooldown = 60f;
+        }
+
+        public override void PlayDeath()
+        {
+            TryPlaySound("Play_AtsukoSeeker_Defeat", 1.6f, true);
+        }
+
+        public override void PlayTeleporterFinish()
+        {
+            TryPlaySound("Play_AtsukoSeeker_Victory", 1.6f, false);
+        }
+
+        public override void PlayTeleporterStart()
+        {
+            TryPlaySound("Play_AtsukoSeeker_TeleStart",3f, false);
+        }
+
+        public override void PlayDamageBlockedServer()
+        {
+            if (!NetworkServer.active || blockedCooldown > 0f) return;
+            bool played = TryPlayNetworkSound(nseBlock, 0.55f, false);
+            if (played) blockedCooldown = 30f;
+        }
+
+        public void PlayAcquireScepter()
+        {
+            if (acquiredScepter) return;
+            TryPlaySound("Play_AtsukoSeeker_AcquireScepter", 21f, true);
+            acquiredScepter = true;
+        }
+
+        public void PlayAcquireLegendary()
+        {
+            if (Util.CheckRoll(50f))
+            {
+                if (Util.CheckRoll(50f))
+                {
+                    TryPlaySound("Play_AtsukoSeeker_Relationship_1", 3.5f, false);
+                }
+                else
+                {
+                    TryPlaySound("Play_AtsukoSeeker_Relationship_2", 5.1f, false);
+                }
+            }
+            else
+            {
+                if (Util.CheckRoll(50f))
+                {
+                    TryPlaySound("Play_AtsukoSeeker_Relationship_3", 7.6f, false);
+                }
+                else
+                {
+                    TryPlaySound("Play_AtsukoSeeker_Relationship_4", 7.2f, false);
+                }
+            }
+        }
+
+        public void PlayFlowerItem()
+        {
+            TryPlaySound("Play_AtsukoSeeker_Flower", 3.6f, false);
+        }
+
+        public void PlayBadItem()
+        {
+            TryPlaySound("Play_AtsukoSeeker_BadItem", 1f, false);
+        }
+
+        protected override void Inventory_onItemAddedClient(ItemIndex itemIndex)
+        {
+            base.Inventory_onItemAddedClient(itemIndex);
+            if (scepterIndex != ItemIndex.None && itemIndex == scepterIndex)
+            {
+                PlayAcquireScepter();
+            }
+            else
+            {
+                ItemDef id = ItemCatalog.GetItemDef(itemIndex);
+                if (id == RoR2Content.Items.TPHealingNova || id == RoR2Content.Items.RepeatHeal)
+                {
+                    PlayFlowerItem();
+                }
+                else
+                if (id == RoR2Content.Items.Squid || id == RoR2Content.Items.Plant || id == RoR2Content.Items.SlowOnHit)
+                {
+                    PlayBadItem();
+                }
+                else if (id && id.deprecatedTier == ItemTier.Tier3)
+                {
+                    PlayAcquireLegendary();
+                }
+            }
+        }
+
+        public override void PlaySecondaryAuthority(GenericSkill skill)
+        {
+            TryPlayNetworkSound(nseShout, 0f, false);
+        }
+
+        public override void PlayUtilityAuthority(GenericSkill skill)
+        {
+            if (Util.CheckRoll(75f))
+            {
+                TryPlayNetworkSound(nseMove, 1f, false);
+            }
+            else
+            {
+                TryPlayNetworkSound(nseMoveLong, 1.4f, false);
+            }
+        }
+
+        public override void PlaySpecialAuthority(GenericSkill skill)
+        {
+            if (specialCooldown > 0f) return;
+
+            if (Util.CheckRoll(0.5f))
+            {
+                if (Util.CheckRoll(1f / 3f))
+                {
+                    TryPlayNetworkSound(nseEx1, 3.3f, false);
+                }
+                else
+                {
+                    if (Util.CheckRoll(0.5f))
+                    {
+                        TryPlayNetworkSound(nseEx2, 2.8f, false);
+                    }
+                    else
+                    {
+                        TryPlayNetworkSound(nseEx3, 1.7f, false);
+                    }
+                }
+            }
+            else
+            {
+                if (Util.CheckRoll(1f / 3f))
+                {
+                    TryPlayNetworkSound(nseExL1, 2.2f, false);
+                }
+                else
+                {
+                    if (Util.CheckRoll(0.5f))
+                    {
+                        TryPlayNetworkSound(nseExL2, 1.9f, false);
+                    }
+                    else
+                    {
+                        TryPlayNetworkSound(nseExL3, 3f, false);
+                    }
+                }
+            }
+                
+            specialCooldown = 10f;
+        }
+
+        public override void PlaySpawn()
+        {
+            TryPlaySound("Play_AtsukoSeeker_Spawn", 1.4f, true);
+        }
+
+        public override void PlayHurt(float percentHPLost)
+        {
+            if (percentHPLost >= 0.1f)
+            {
+                TryPlaySound("Play_AtsukoSeeker_Hurt", 0f, false);
+            }
+        }
+
+        public override void PlayLowHealth()
+        {
+            if (lowHealthCooldown > 0f) return;
+            bool playedSound;
+
+            if (Util.CheckRoll(50f))
+            {
+                playedSound = TryPlaySound("Play_AtsukoSeeker_LowHealth_1", 2.4f, false);
+            }
+            else
+            {
+                playedSound = TryPlaySound("Play_AtsukoSeeker_LowHealth_2", 4f, false);
+            }
+
+            if (playedSound) lowHealthCooldown = 60f;
+        }
+
+        public override void PlayVictory()
+        {
+            TryPlaySound("Play_AtsukoSeeker_Memorial", 24f, true);
+        }
+
+        public override void PlayShrineOfChanceFailServer()
+        {
+            if (shrineFailCooldown > 0f) return;
+            if (Util.CheckRoll(15f))
+            {
+                bool played;
+                played = TryPlayNetworkSound(nseShrineFail, 1.2f, false);
+                if (played) shrineFailCooldown = 60f;
+            }
+        }
+
+        public override bool ComponentEnableVoicelines()
+        {
+            return AtsukoSeekerVoiceover.enableVoicelines.Value;
+        }
+    }
+}
---
+++ AtsukoSeekerVoiceover/Plugin.cs
@@ -0,0 +1,227 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using System;
+using UnityEngine.AddressableAssets;
+using UnityEngine;
+using RoR2;
+using AtsukoSeekerVoiceover.Modules;
+using BaseVoiceoverLib;
+using System.Collections.Generic;
+using AtsukoSeekerVoiceover.Components;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security.Permissions;
+using System.Security;
+
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+namespace AtsukoSeekerVoiceover
+{
+    [BepInDependency(R2API.SoundAPI.PluginGUID)]
+    [BepInDependency("com.rune580.riskofoptions", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInDependency("com.Moffein.BaseVoiceoverLib", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("com.Schale.AtsukoSeekerVoiceover", "AtsukoSeekerVoiceover", "1.0.0")]
+    public class AtsukoSeekerVoiceover : BaseUnityPlugin
+    {
+        public static ConfigEntry<bool> enableVoicelines;
+        public static bool playedSeasonalVoiceline = false;
+        public static AssetBundle assetBundle;
+        public static SurvivorDef survivorDef = Addressables.LoadAssetAsync<SurvivorDef>("RoR2/DLC2/Seeker/Seeker.asset").WaitForCompletion();
+
+        private void Awake()
+        {
+            Files.PluginInfo = this.Info;
+            RoR2.RoR2Application.onLoad += OnLoad;
+            new Content().Initialize();
+
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("AtsukoSeekerVoiceover.atsukoseekerbundle"))
+            {
+                assetBundle = AssetBundle.LoadFromStream(stream);
+            }
+
+            SoundBanks.Init();
+
+            InitNSE();
+
+            enableVoicelines = base.Config.Bind<bool>(new ConfigDefinition("Settings", "Enable Voicelines"), true, new ConfigDescription("Enable voicelines when using the Atsuko Seeker Skin."));
+            enableVoicelines.SettingChanged += EnableVoicelines_SettingChanged;
+
+            if (BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("com.rune580.riskofoptions"))
+            {
+                RiskOfOptionsCompat();
+            }
+        }
+
+        private void EnableVoicelines_SettingChanged(object sender, EventArgs e)
+        {
+            RefreshNSE();
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private void RiskOfOptionsCompat()
+        {
+            RiskOfOptions.ModSettingsManager.SetModIcon(assetBundle.LoadAsset<Sprite>("atsuko_grin"));
+            RiskOfOptions.ModSettingsManager.AddOption(new RiskOfOptions.Options.CheckBoxOption(enableVoicelines));
+        }
+
+        private void OnLoad()
+        {
+            SkinDef atsukoSkin = null;
+            SkinDef atsukoSkin2 = null;
+            SkinDef[] skins = SkinCatalog.FindSkinsForBody(BodyCatalog.FindBodyIndex("SeekerBody"));
+            foreach (SkinDef skinDef in skins)
+            {
+                if (skinDef.name == "AtsukoSkinDef")
+                {
+                    atsukoSkin = skinDef;
+                }
+                else if (skinDef.name == "AtsukoSkinDefMaskless")
+                {
+                    atsukoSkin2 = skinDef;
+                }
+
+                if (atsukoSkin && atsukoSkin2) break;
+            }
+
+            if (!atsukoSkin)
+            {
+                Debug.LogError("AtsukoSeekerVoiceover: Atsuko Seeker SkinDef not found. Voicelines will not work!");
+            }
+            else
+            {
+                VoiceoverInfo voiceoverInfo = new VoiceoverInfo(typeof(AtsukoSeekerVoiceoverComponent), atsukoSkin, "SeekerBody");
+                voiceoverInfo.selectActions += AtsukoSelect;
+            }
+
+            if (!atsukoSkin2)
+            {
+                Debug.LogError("AtsukoSeekerVoiceover: Atsuko Seeker (Maskless) SkinDef not found. Voicelines will not work!");
+            }
+            else
+            {
+                VoiceoverInfo voiceoverInfo = new VoiceoverInfo(typeof(AtsukoSeekerVoiceoverComponent), atsukoSkin2, "SeekerBody");
+                voiceoverInfo.selectActions += AtsukoSelect;
+            }
+
+            RefreshNSE();
+        }
+
+        private void AtsukoSelect(GameObject mannequinObject)
+        {
+            if (!enableVoicelines.Value) return;
+
+            bool played = false;
+            if (!playedSeasonalVoiceline)
+            {
+                if (System.DateTime.Today.Month == 1 && System.DateTime.Today.Day == 1)
+                {
+                    Util.PlaySound("Play_AtsukoSeeker_Lobby_Newyear", mannequinObject);
+                    played = true;
+                }
+                else if (System.DateTime.Today.Month == 1 && System.DateTime.Today.Day == 20)
+                {
+                    Util.PlaySound("Play_AtsukoSeeker_Lobby_bday", mannequinObject);
+                    played = true;
+                }
+                else if (System.DateTime.Today.Month == 10 && System.DateTime.Today.Day == 31)
+                {
+                    Util.PlaySound("Play_AtsukoSeeker_Lobby_Halloween", mannequinObject);
+                    played = true;
+                }
+                else if (System.DateTime.Today.Month == 12 && System.DateTime.Today.Day == 25)
+                {
+                    Util.PlaySound("Play_AtsukoSeeker_Lobby_xmas", mannequinObject);
+                    played = true;
+                }
+
+                if (played) playedSeasonalVoiceline = true;
+            }
+            if (!played)
+            {
+                if (Util.CheckRoll(5f))
+                {
+                    Util.PlaySound("Play_AtsukoSeeker_TitleDrop", mannequinObject);
+                }
+                else
+                {
+                    Util.PlaySound("Play_AtsukoSeeker_Lobby", mannequinObject);
+                }
+            }
+        }
+
+        private void InitNSE()
+        {
+            AtsukoSeekerVoiceoverComponent.nseBlock = RegisterNSE("Play_AtsukoSeeker_Muda");
+            AtsukoSeekerVoiceoverComponent.nseShout = RegisterNSE("Play_AtsukoSeeker_Shout");
+            AtsukoSeekerVoiceoverComponent.nseShrineFail = RegisterNSE("Play_AtsukoSeeker_ShrineFail");
+            AtsukoSeekerVoiceoverComponent.nseMove = RegisterNSE("Play_AtsukoSeeker_Move");
+            AtsukoSeekerVoiceoverComponent.nseMoveLong = RegisterNSE("Play_AtsukoSeeker_Move_Long");
+            AtsukoSeekerVoiceoverComponent.nseEx1 = RegisterNSE("Play_AtsukoSeeker_ExSkill_1");
+            AtsukoSeekerVoiceoverComponent.nseEx2 = RegisterNSE("Play_AtsukoSeeker_ExSkill_2");
+            AtsukoSeekerVoiceoverComponent.nseEx3 = RegisterNSE("Play_AtsukoSeeker_ExSkill_3");
+
+            AtsukoSeekerVoiceoverComponent.nseExL1 = RegisterNSE("Play_AtsukoSeeker_ExSkill_Level_1");
+            AtsukoSeekerVoiceoverComponent.nseExL2 = RegisterNSE("Play_AtsukoSeeker_ExSkill_Level_2");
+            AtsukoSeekerVoiceoverComponent.nseExL3 = RegisterNSE("Play_AtsukoSeeker_ExSkill_Level_3");
+        }
+
+        public void RefreshNSE()
+        {
+            foreach (NSEInfo nse in nseList)
+            {
+                nse.ValidateParams();
+            }
+        }
+
+        private NetworkSoundEventDef RegisterNSE(string eventName)
+        {
+            NetworkSoundEventDef nse = ScriptableObject.CreateInstance<NetworkSoundEventDef>();
+            nse.eventName = eventName;
+            Content.networkSoundEventDefs.Add(nse);
+            nseList.Add(new NSEInfo(nse));
+            return nse;
+        }
+
+        public static List<NSEInfo> nseList = new List<NSEInfo>();
+        public class NSEInfo
+        {
+            public NetworkSoundEventDef nse;
+            public uint akId = 0u;
+            public string eventName = string.Empty;
+
+            public NSEInfo(NetworkSoundEventDef source)
+            {
+                this.nse = source;
+                this.akId = source.akId;
+                this.eventName = source.eventName;
+            }
+
+            private void DisableSound()
+            {
+                nse.akId = 0u;
+                nse.eventName = string.Empty;
+            }
+
+            private void EnableSound()
+            {
+                nse.akId = this.akId;
+                nse.eventName = this.eventName;
+            }
+
+            public void ValidateParams()
+            {
+                if (this.akId == 0u) this.akId = nse.akId;
+                if (this.eventName == string.Empty) this.eventName = nse.eventName;
+
+                if (!enableVoicelines.Value)
+                {
+                    DisableSound();
+                }
+                else
+                {
+                    EnableSound();
+                }
+            }
+        }
+    }
+}
