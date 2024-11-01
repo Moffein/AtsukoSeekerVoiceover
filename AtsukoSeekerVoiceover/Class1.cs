6bf4b960b186ef00f95b229eeec099dee6fc968c
Moffein <22742925+Moffein@users.noreply.github.com>
2024-11-01 11:33:01 -0500

    Add project files.

---
+++ AtsukoSeekerVoiceover/Class1.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using System;
+
+namespace AtsukoSeekerVoiceover
+{
+    [BepInDependency(R2API.SoundAPI.PluginGUID)]
+    [BepInDependency("com.rune580.riskofoptions", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInDependency("com.Moffein.BaseVoiceoverLib", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin("com.Schale.AtsukoSeekerVoiceover", "AtsukoSeekerVoiceover", "1.0.0")]
+    public class AtsukoSeekerVoiceover : BaseUnityPlugin
+    {
+
+    }
+}
